afff0ca447b1570c2033e0b081aa38b36c45df77
abhishekgupta9595 <148917898+abhishekgupta9595@users.noreply.github.com>
2023-10-25 04:58:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import numpy as np
 import os
 print(os.getcwd())
 # loading the saved models
-heart_disease_model_lr = pickle.load(open('logistic_model_pkl','rb'))
+heart_disease_model_lr = pickle.load(open('/mount/src/heart_disease/logistic_model_pkl','rb'))
 print('heart_disease_model_lr loaded')
-heart_disease_model_dt = pickle.load(open('DecisionTreeClassifier','rb'))
+heart_disease_model_dt = pickle.load(open('/mount/src/heart_disease/DecisionTreeClassifier.pkl','rb'))
 print('heart_disease_model_dt loaded')
-heart_disease_model_xgb = pickle.load(open('XGBoost.pkl','rb'))
+heart_disease_model_xgb = pickle.load(open('/mount/src/heart_disease/XGBoost.pkl','rb'))
 print('heart_disease_model_xgb loaded')
 
 # sidebar for navigation
